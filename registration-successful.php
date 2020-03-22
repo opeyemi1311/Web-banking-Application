867acd17ab18bfa527d4f315358ed8b0b17562c2
opeyemi1311 <40266522+opeyemi1311@users.noreply.github.com>
2020-03-22 15:53:22 +0100

    Add files via upload
    All the protected pages with session, login , signup, transaction are included in the zip file named prot. all users need to do is unzip the file, create database and tables as indicated in the scripts

---
+++ registration-successful.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>InvestInEverydayPay| Invest and Get paid Daily</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Invest and get paid Daily">
+	<meta name="keywords" content="invest, paid, profit,online business, good business, InvestInEverydayPay">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap" rel="stylesheet">
+ 
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- social media dependency-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+	<!-- Social Media styles-->
+
+	<style>
+		.fa {
+			border-radius: 25px;
+		  padding: 5px;
+		  font-size: 30px;
+		  width: 50px;
+		  text-align: center;
+		  text-decoration: none;
+		  margin: 5px 2px;
+		}
+		
+		.fa:hover {
+			opacity: 0.7;
+		}
+		
+		.fa-facebook {
+		  background: #3B5998;
+		  color: white;
+		}
+		
+		.fa-twitter {
+		  background: #55ACEE;
+		  color: white;
+		}
+		
+		
+		
+		.fa-youtube {
+		  background: #bb0000;
+		  color: white;
+		}
+		
+		.fa-instagram {
+		  background: #125688;
+		  color: white;
+		}
+		
+		
+		</style>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header Section -->
+	<header class="header-section">
+		<a href="index.html" class="site-logo">
+			<img src="img/logo.png" alt="">
+		</a>
+		<nav class="header-nav">
+			<ul class="main-menu">
+				<li><a href="index.html" class="active">Home</a></li>
+				<li><a href="about-us.html">About Us</a></li>
+				<li><a href="#">Portfolio</a>
+					<ul class="sub-menu">
+						<li><a href="about-us.html">Packages</a></li>
+						<li><a href="investment.html">Investment</a></li>
+						<li><a href="#">News</a></li>
+					</ul>
+				</li>
+				
+				<li><a href="contact.html">Contact</a></li>
+				<li><a href="prot/login.php">Login</a></li>
+			</ul>
+			<div class="header-right">
+			<!--	<a href="#" class="hr-btn"><i class="flaticon-029-telephone-1"></i>Call us now! </a> -->
+				<a href="prot/signup.php"><div class="hr-btn hr-btn-2">Register</div> </a>
+			</div>
+		</nav>
+	</header>
+	<!-- Header Section end -->
+
+	<!-- Page top Section end -->
+	<section class="page-top-section set-bg" data-setbg="img/page-top-bg/4.jpg">
+		<div class="container">
+			<h3 style="color: white;">You have Registered Successfully </h3>
+			
+			<nav class="site-breadcrumb">
+				
+			<a href="prot/login.php">	<button class="site-btn mb-2 mr-2">Login</button> </a>
+				
+			</nav>
+		</div>
+	</section>
+	<!-- Page top Section end -->
+
+	
+	<!-- Footer Section -->
+	<footer class="footer-section">
+		<div class="container">
+			<a href="index.html" class="footer-logo">
+				<img src="img/logo.png" alt="">
+			</a>
+			<div class="row">
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget">
+						<h2>Portfolio</h2>
+						<ul>
+							
+							<li><a href="about-us.html">Packages</a></li>
+							<li><a href="loans.html">Investment</a></li>
+							<li><a href="elements.html">News</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget">
+						<h2>About us</h2>
+						<ul>
+							<li><a href="#">About us</a></li>
+							<li><a href="#">Our story</a></li>
+							<li><a href="#">Meet the board</a></li>
+							<li><a href="#">Careers</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget">
+						<h2>Legal</h2>
+						<ul>
+							<li><a href="#">Privacy policy</a></li>
+							<li><a href="#">Our principles</a></li>
+							<li><a href="#">Website terms</a></li>
+							
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="footer-widget">
+						<h2>Site Info</h2>
+						<ul>
+							<li><a href="#">Support</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Sitemap</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<!-- Add font awesome icons -->
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+
+		
+			<p>All right reserverd</p>
+			<div class="copyright">
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |  InvestInEverydayPay
+</div>
+		</div>
+	</footer>
+	<!-- Footer Section end -->
+	
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/jquery-ui.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
